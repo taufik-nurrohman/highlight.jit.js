d3864da471730193192a8884bafcacad632f8a6c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-06-05 06:11:40 +0700

    TODO: Prioritize Class Name Prefixed by lang- and language-

--- highlight.jit.js
+++ highlight.jit.js
@@ -18,9 +18,16 @@
         'before:highlightBlock': ({block, language}) => {
             // `language` is `undefined` if no language matched to a class name in the block
             if (!language) {
-                // TODO: Prioritize class name using format `lang-*` and `language-*`
-                let guessLanguages = (block.className || "").split(/\s+/);
+                let classes = block.className || "",
+                    // Prioritize class name using format `lang-*` and `language-*`
+                    // TODO: Get language regex from the option where possible
+                    guessLanguage = classes.match(/\blang(?:uage)?-([\w-:.]+)\b/),
+                    // Else, try to find the original language name from every class names
+                    guessLanguages = guessLanguage ? [guessLanguage[1]] : classes.split(/\s+/);
                 guessLanguages.forEach(language => {
+                    if (!language) {
+                        return;
+                    }
                     let script = doc.createElement('script');
                     script.src = hljs.currentLanguagePath.replace(/%s/, language);
                     doc.body.appendChild(script);
@@ -30,6 +37,7 @@
                     }, false);
                     // Highlight on success
                     script.addEventListener('load', () => {
+                        // TODO: Prevent recursive `before:highlightBlock` hook here
                         hljs.highlightBlock(block);
                     }, false);
                 });
